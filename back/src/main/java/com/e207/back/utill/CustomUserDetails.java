e7deb4625ca18bc329d7227e3e37f4345ddd5d02
최명성 <audtjd1893@gmail.com>
2024-04-11 15:56:06 +0900

    Feat: 스프링 시큐리티를 통한 JWT 발급

---
+++ back/src/main/java/com/e207/back/utill/CustomUserDetails.java
@@ -0,0 +1,17 @@
+package com.e207.back.utill;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CustomUserDetails {
+//    private int userSeq;
+    private String nickname;
+//    private String sex;
+//    private int birth;
+    private String userId;
+}
+
