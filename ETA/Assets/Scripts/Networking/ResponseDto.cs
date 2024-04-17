6662f5f03f4d2c0b74612fc4d23c68fc580cbdd2
장수영 <suyoung998@naver.com>
2024-04-17 19:50:09 +0900

    Feat: 파일 구조 변경

---
+++ ETA/Assets/Scripts/Networking/ResponseDto.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ResponseDto : MonoBehaviour
+{
+    public string id;
+    public string nickname;
+    public string accessToken;
+    public int gold;
+}
