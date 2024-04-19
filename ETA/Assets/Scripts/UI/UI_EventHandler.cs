8ba33c733c1e1909053916f8b93f17bdb6797851
HocheolHwang <inpofes7849@naver.com>
2024-04-19 10:29:25 +0900

    Comment: UI_EventHandler 스크립트에 주석 추가

--- ETA/Assets/Scripts/UI/UI_EventHandler.cs
+++ ETA/Assets/Scripts/UI/UI_EventHandler.cs
@@ -4,10 +4,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/// <summary>
+/// UI 이벤트 처리를 담당하는 클래스
+/// </summary>
 public class UI_EventHandler : MonoBehaviour, IPointerClickHandler, IDragHandler
 {
-
+    // 클릭 이벤트 발생 시 호출되는 액션
     public Action<PointerEventData> OnClickHandler = null;
+
+    // 드래그 이벤트 발생 시 호출되는 액션
     public Action<PointerEventData> OnDragHandler = null;
 
     public void OnDrag(PointerEventData eventData)
