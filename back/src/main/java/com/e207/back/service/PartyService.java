156acdee28cd9b94205f2765c8adadebaf4f7681
최명성 <audtjd1893@gmail.com>
2024-04-21 16:30:38 +0900

    Feat: 파티 참가 API 추가

--- back/src/main/java/com/e207/back/service/PartyService.java
+++ back/src/main/java/com/e207/back/service/PartyService.java
@@ -1,10 +1,13 @@
 package com.e207.back.service;
 
 import com.e207.back.dto.request.CreatePartyRequestDto;
+import com.e207.back.dto.request.EnterPartyRequestDto;
 import com.e207.back.dto.response.CreatePartyResponseDto;
+import com.e207.back.dto.response.EnterPartyResponseDto;
 import org.springframework.http.ResponseEntity;
 
 public interface PartyService {
 
     ResponseEntity<? super CreatePartyResponseDto> createParty(CreatePartyRequestDto dto);
+    ResponseEntity<? super EnterPartyResponseDto> enterParty(EnterPartyRequestDto dto);
 }
