eb5aca0579e70cc48829a3038ae2492915467679
HocheolHwang <inpofes7849@naver.com>
2024-04-23 11:20:28 +0900

    Feat: 버튼에 커서 모양 변경 기능 추가

--- ETA/Assets/Scripts/UI/Cursor/HandCursorChanger.cs
+++ ETA/Assets/Scripts/UI/Cursor/HandCursorChanger.cs
@@ -7,32 +7,33 @@ using UnityEngine.EventSystems;
 /// </summary>
 public class HandCursorChanger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
-    //private static Texture2D handCursor;
+    // 손가락 커서 이미지를 저장하는 변수
+    private static Texture2D handCursor;
 
-    //private void Start()
-    //{
-    //    handCursor = Resources.Load<Texture2D>("Sprites/7. Common Sprites/Cursor Sprites/Hand Cursor");
-    //    // handCursor = Resources.Load<Texture2D>("Hand Cursor");
-    //}
-
-    [Header("[ 커서 이미지 ]")]
-    public Texture2D handCursor; // 손가락 모양의 커서 이미지
+    private void Start()
+    {
+        // 손가락 커서 이미지를 Resources에서 로드하여 handCursor 변수에 저장
+        handCursor = Resources.Load<Texture2D>("Sprites/7. Common Sprites/Cursor Sprites/Hand Cursor");
+    }
 
     // 마우스가 UI 오브젝트에 들어갈 때 호출되는 메서드
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Cursor.SetCursor(handCursor, Vector2.zero, CursorMode.Auto); // 마우스 커서를 손가락 모양으로 변경
+        // 마우스 커서를 손가락 모양으로 변경
+        Cursor.SetCursor(handCursor, Vector2.zero, CursorMode.Auto);
     }
 
     // 마우스가 UI 오브젝트에서 나갈 때 호출되는 메서드
     public void OnPointerExit(PointerEventData eventData)
     {
-        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto); // 마우스 커서를 기본 모양으로 변경
+        // 마우스 커서를 기본 모양으로 변경
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
 
     // UI 오브젝트를 클릭했을 때 호출되는 메서드
     public void OnPointerClick(PointerEventData eventData)
     {
-        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto); // 마우스 커서를 기본 모양으로 변경
+        // 마우스 커서를 기본 모양으로 변경
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
 }
