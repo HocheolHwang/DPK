5f339f71e7fae463a1c9b1d9338591c478881d43
HocheolHwang <inpofes7849@naver.com>
2024-04-23 20:05:29 +0900

    Feat: UI prefab 폴더 구조 변경 및 이름 변경

--- ETA/Assets/Scripts/UI/Popup/LoginPopupUI.cs
+++ ETA/Assets/Scripts/UI/Popup/LoginPopupUI.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class LoginFixedUI : UI_Fixed
+public class LoginPopupUI : UI_Popup
 {
+    // 버튼 인덱스
+    enum Buttons
+    {
+        LoginButton,
+        SwitchSignupButton
+    }
+
     // 로그인 UI 초기화
     public override void Init()
     {
         base.Init(); // 기본 초기화
+
+        // 버튼 바인딩
+        Bind<Button>(typeof(Buttons));
 
         // 로그인 시도 버튼 이벤트 등록
         GameObject loginButton = GetObject((int)Buttons.LoginButton);
@@ -29,14 +40,12 @@ public class LoginFixedUI : UI_Fixed
     // 회원가입 전환 버튼 클릭 이벤트 핸들러
     private void OnSwitchSignupClicked(PointerEventData data)
     {
-        // 회원가입 Fixed UI를 띄움
-        Managers.UI.ShowFixedUI<SignupFixedUI>("Signup Fixed UI");
-    }
+        // 현재 Popup UI를 닫음
+        ClosePopupUI();
 
-    // 버튼 인덱스
-    enum Buttons
-    {
-        LoginButton,
-        SwitchSignupButton
+        Debug.Log("회원가입 창으로 변경!");
+
+        // 회원가입 Popup UI를 띄움
+        Managers.UI.ShowPopupUI<SignupPopupUI>("[Login] Signup Popup UI");
     }
 }
--- ETA/Assets/Scripts/UI/Popup/SignupPopupUI.cs
+++ ETA/Assets/Scripts/UI/Popup/SignupPopupUI.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class SignupFixedUI : UI_Fixed
+public class SignupPopupUI : UI_Popup
 {
+    // 버튼 인덱스
+    enum Buttons
+    {
+        SignupButton,
+        SwitchLoginButton
+    }
+
     // 회원가입 UI 초기화
     public override void Init()
     {
         base.Init(); // 기본 초기화
+
+        // 버튼 바인딩
+        Bind<Button>(typeof(Buttons));
 
         // 회원가입 시도 버튼 이벤트 등록
         GameObject signupButton = GetObject((int)Buttons.SignupButton);
@@ -29,14 +40,12 @@ public class SignupFixedUI : UI_Fixed
     // 로그인 전환 버튼 클릭 이벤트 핸들러
     private void OnSwitchLoginClicked(PointerEventData data)
     {
-        // 로그인 Fixed UI를 띄움
-        Managers.UI.ShowFixedUI<LoginFixedUI>("Login Fixed UI");
-    }
+        // 현재 Popup UI를 닫음
+        ClosePopupUI();
 
-    // 버튼 인덱스
-    enum Buttons
-    {
-        SignupButton,
-        SwitchLoginButton
+        Debug.Log("로그인 창으로 변경!");
+
+        // 로그인 Popup UI를 띄움
+        Managers.UI.ShowPopupUI<LoginPopupUI>("[Login] Login Popup UI");
     }
 }
