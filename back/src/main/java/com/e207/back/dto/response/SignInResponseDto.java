aec3ede4170a34927803f4c00114e598fec51706
최명성 <audtjd1893@gmail.com>
2024-04-23 17:25:19 +0900

    Feat: 로그인 했을 때 소지 골드 보내주기

--- back/src/main/java/com/e207/back/dto/response/SignInResponseDto.java
+++ back/src/main/java/com/e207/back/dto/response/SignInResponseDto.java
@@ -13,19 +13,21 @@ import org.springframework.http.ResponseEntity;
 public class SignInResponseDto extends ResponseDto {
     String accessToken;
     boolean isFirst;
-    SignInResponseDto(String accessToken, boolean isFirst){
+    long playerGold;
+    SignInResponseDto(String accessToken, boolean isFirst, long playerGold){
         super();
         this.accessToken = accessToken;
         this.isFirst = isFirst;
+        this.playerGold = playerGold;
     }
 
-    public static ResponseEntity<? super SignInResponseDto> success(String accessToken, boolean isFirst) {
-        SignInResponseDto responseBody = new SignInResponseDto(accessToken, isFirst);
+    public static ResponseEntity<? super SignInResponseDto> success(String accessToken, boolean isFirst, long playerGold) {
+        SignInResponseDto responseBody = new SignInResponseDto(accessToken, isFirst, playerGold);
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     }
 
     public static ResponseEntity<? super SignInResponseDto> playerPasswordValidationFail() {
-        SignInResponseDto responseBody = new SignInResponseDto("", false);
+        SignInResponseDto responseBody = new SignInResponseDto("", false, 0);
         responseBody.setMessage("비밀번호가 틀렸습니다.");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
     }
