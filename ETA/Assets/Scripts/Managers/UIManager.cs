4a0cb052029531c9db6025eb48ee79f9473c6857
최명성 <audtjd1893@gmail.com>
2024-04-24 14:40:21 +0900

    Feat: 체력바 UI 추가

--- ETA/Assets/Scripts/Managers/UIManager.cs
+++ ETA/Assets/Scripts/Managers/UIManager.cs
@@ -49,6 +49,23 @@ public class UIManager
             canvas.sortingOrder = 0;
         }
     }
+    public T MakeWorldSpaceUI<T>(Transform parent = null, string name = null) where T : UI_Base
+    {
+        if (string.IsNullOrEmpty(name))
+            name = typeof(T).Name;
+
+        GameObject go = Managers.Resource.Instantiate($"UI/WorldSpace/{name}");
+        if (parent != null)
+            go.transform.SetParent(parent);
+
+        Canvas canvas = go.GetOrAddComponent<Canvas>();
+        canvas.renderMode = RenderMode.WorldSpace;
+        canvas.worldCamera = Camera.main;
+
+        return Util.GetOrAddComponent<T>(go);
+    }
+
+
 
     // UI 하위 요소를 동적으로 생성하는 메서드
     public T MakeSubItem<T>(Transform parent = null, string name = null) where T : UI_Base
