a0f9be33a0680cde112ed0c72ae96ae228f7b9c8
HocheolHwang <inpofes7849@naver.com>
2024-04-25 00:21:10 +0900

    Feat: 로딩중 팝업 창 비활성화

--- ETA/Assets/Scripts/Scene/Login_Scene.cs
+++ ETA/Assets/Scripts/Scene/Login_Scene.cs
@@ -8,5 +8,6 @@ public class Login_Scene : MonoBehaviour
     {
         Managers.UI.ShowPopupUI<Before_Login_Popup_UI>("[Login]_Before_Login_Popup_UI");
         Managers.UI.ShowPopupUI<Login_Popup_UI>("[Login]_Login_Popup_UI");
+        // Managers.UI.ShowPopupUI<Login_Loading_Popup_UI>("[Login]_Login_Loading_Popup_UI");
     }
 }
