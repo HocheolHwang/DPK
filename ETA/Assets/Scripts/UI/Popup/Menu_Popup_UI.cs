f3b91446bb2d5748e166ed21a879f736c694e5d0
HocheolHwang <inpofes7849@naver.com>
2024-04-25 19:10:03 +0900

    Feat: (임시) 메뉴창 prefab 및 스크립트 추가

---
+++ ETA/Assets/Scripts/UI/Popup/Menu_Popup_UI.cs
@@ -0,0 +1,49 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class Menu_Popup_UI : UI_Popup
+{
+    // 버튼 인덱스
+    enum Buttons
+    {
+        Cancel_Button,
+        Game_Exit_Button
+    }
+
+    // 로그인 UI 초기화
+    public override void Init()
+    {
+        base.Init(); // 기본 초기화
+
+        // 버튼 바인딩
+        Bind<Button>(typeof(Buttons));
+
+        // 남아 있기 버튼 이벤트 등록
+        Button cancelButton = GetButton((int)Buttons.Cancel_Button);
+        AddUIEvent(cancelButton.gameObject, Cancel);
+        AddUIKeyEvent(cancelButton.gameObject, () => Cancel(null), KeyCode.Escape);
+
+        // 게임 종료 버튼 이벤트 등록
+        Button gameExitButton = GetButton((int)Buttons.Game_Exit_Button);
+        AddUIEvent(gameExitButton.gameObject, GameExit);
+        AddUIKeyEvent(gameExitButton.gameObject, () => GameExit(null), KeyCode.Return);
+    }
+
+    // 남아 있기
+    private void Cancel(PointerEventData data)
+    {
+        // 현재 Popup UI를 닫음
+        ClosePopupUI();
+    }
+
+    // 게임 종료
+    private void GameExit(PointerEventData data)
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // 어플리케이션 종료
+#endif
+    }
+}
