e478bb95b843d5cdaf2b55046baee13a51838ba7
신현기 <muffin3864@naver.com>
2024-04-28 23:30:50 +0900

    FEAT: tutorial structure and active done

---
+++ ETA/Assets/Scripts/Tutorial/TutorialTrigger.cs
@@ -0,0 +1,14 @@
+using Highlands;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private Transform triggerObject;
+
+    public bool isTrigger { set; get; } = false;
+
+    
+}
