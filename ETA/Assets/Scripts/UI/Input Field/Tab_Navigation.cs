fe665a225d501749ac67fdf8e8e73b2ab08173c6
HocheolHwang <inpofes7849@naver.com>
2024-05-01 17:46:20 +0900

    Feat: 코드 리펙토링
    진 행 중 !

--- ETA/Assets/Scripts/UI/Input Field/Tab_Navigation.cs
+++ ETA/Assets/Scripts/UI/Input Field/Tab_Navigation.cs
@@ -3,9 +3,8 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 /// <summary>
-/// TabNavigation 클래스는 Unity UI에서 Tab 키를 이용한 입력 필드 간의 순환 이동을 가능하게 함.
-/// 사용자는 Tab 키를 눌러 지정된 입력 필드 배열(inputs) 내의 다음 필드로 포커스를 이동시킬 수 있음.
-/// 배열의 마지막 입력 필드에서 Tab을 누르면, 배열의 첫 번째 입력 필드로 포커스가 이동함.
+/// Input Field UI에서 Tab 키를 이용해 Input Field 간의 순환 이동을 가능하게 하는 컴포넌트입니다.
+/// 배열의 마지막 Input Field에서 Tab을 누르면, 배열의 첫 번째 Input Field로 포커스가 이동합니다.
 /// </summary>
 public class Tab_Navigation : MonoBehaviour
 {
@@ -15,7 +14,7 @@ public class Tab_Navigation : MonoBehaviour
 
     void Update()
     {
-        // PopUp이 띄워저 있는지 확인
+        // Popup UI가 띄워저 있는지 확인
         if (Managers.UI.GetTopPopupUI() != null)
         {
             // PopUp이 띄워져있다면 가장 위에 띄워져있는 Popup인지 확인
@@ -36,16 +35,16 @@ public class Tab_Navigation : MonoBehaviour
                     int index = System.Array.IndexOf(inputs, current);
                     if (index >= 0)
                     {
-                        // 현재 선택된 입력 필드의 다음 입력 필드를 계산
-                        // 배열의 마지막 입력 필드에서 Tab을 누르면, 첫 번째 입력 필드로 돌아감
+                        // 현재 선택된 Input Field의 다음 Input Field를 계산
+                        // 배열의 마지막 Input Field에서 Tab을 누르면, 첫 번째 Input Field로 돌아감
                         Selectable next = inputs[(index + 1) % inputs.Length];
                         if (next != null)
                         {
-                            // 다음 입력 필드가 InputField 컴포넌트를 가지고 있으면, 포인터 클릭 이벤트를 시뮬레이션 함
+                            // 다음 Input Field가 Input Field 컴포넌트를 가지고 있으면, 포인터 클릭 이벤트를 시뮬레이션 함
                             InputField inputfield = next.GetComponent<InputField>();
                             if (inputfield != null) inputfield.OnPointerClick(new PointerEventData(EventSystem.current));
 
-                            // EventSystem을 통해 다음 입력 필드로 포커스를 이동
+                            // 다음 Input Field로 포커스를 이동
                             EventSystem.current.SetSelectedGameObject(next.gameObject, new BaseEventData(EventSystem.current));
                         }
                     }
@@ -53,10 +52,10 @@ public class Tab_Navigation : MonoBehaviour
             }
             else
             {
-                // 현재 선택된 UI 요소가 없을 때의 처리. 예를 들어, 첫 번째 입력 필드로 포커스를 설정할 수 있습니다.
+                // 현재 선택된 UI 요소가 없을 경우
                 if (inputs.Length > 0 && inputs[0] != null)
                 {
-                    // 첫 번째 입력 필드로 포커스를 이동
+                    // 첫 번째 Input Field로 포커스를 이동
                     EventSystem.current.SetSelectedGameObject(inputs[0].gameObject, new BaseEventData(EventSystem.current));
                 }
             }
--- ETA/Assets/Scripts/UI/Popup/Before_Login_Popup_UI.cs
+++ ETA/Assets/Scripts/UI/Popup/Before_Login_Popup_UI.cs
@@ -1,12 +1,13 @@
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Before_Login_Popup_UI : UI_Popup
 {
-    // 로그인 UI 초기화
+    // ------------------------------ UI 초기화 ------------------------------
     public override void Init()
     {
-        base.Init(); // 기본 초기화
+        // 기본 초기화
+        base.Init();
     }
 }
