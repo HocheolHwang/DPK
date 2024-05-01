1d5838cc55306d4027da5ef746212105d9837d8a
신현기 <muffin3864@naver.com>
2024-05-01 20:56:45 +0900

    FEAT: background music add

--- ETA/Assets/Scripts/Scene/Login_Scene.cs
+++ ETA/Assets/Scripts/Scene/Login_Scene.cs
@@ -8,6 +8,7 @@ public class Login_Scene : BaseScene
     {
         Managers.UI.ShowPopupUI<Before_Login_Popup_UI>("[Login]_Before_Login_Popup_UI");
         Managers.UI.ShowPopupUI<Login_Popup_UI>("[Login]_Login_Popup_UI");
+        Managers.Sound.Play("BackgroundMusic/Login");
     }
 
     public override void Clear()
