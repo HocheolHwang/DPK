f81750c697fa65934031fb806ccfe539efaa1d2c
HocheolHwang <inpofes7849@naver.com>
2024-05-05 22:14:06 +0900

    Feat: 파티 만들기 연결 및 경고 텍스트 출력 기능 추가

--- ETA/Assets/Scripts/UI/Popup/Party_Create_Popup_UI.cs
+++ ETA/Assets/Scripts/UI/Popup/Party_Create_Popup_UI.cs
@@ -1,7 +1,7 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
 
 public class Party_Create_Popup_UI : UI_Popup
 {
@@ -19,10 +19,16 @@ public class Party_Create_Popup_UI : UI_Popup
         Party_Name_InputField
     }
 
+    enum Texts
+    {
+        Warning_Text
+    }
+
     // UI 컴포넌트 바인딩 변수
     private Button cancelButton;
     private Button partyCreateButton;
     private TMP_InputField partyNameInputField;
+    private TextMeshProUGUI warning;
 
 
     // ------------------------------ UI 초기화 ------------------------------
@@ -34,6 +40,7 @@ public class Party_Create_Popup_UI : UI_Popup
         // 바인딩
         Bind<Button>(typeof(Buttons));
         Bind<TMP_InputField>(typeof(InputFields));
+        Bind<TextMeshProUGUI>(typeof(Texts));
 
         // 취소하기 버튼 이벤트 등록
         cancelButton = GetButton((int)Buttons.Cancel_Button);
@@ -47,7 +54,12 @@ public class Party_Create_Popup_UI : UI_Popup
 
         // 파티 이름 입력 정보 및 포커스 설정
         partyNameInputField = Get<TMP_InputField>((int)InputFields.Party_Name_InputField);
-        partyNameInputField.OnPointerClick(new PointerEventData(EventSystem.current));
+
+        // 인풋 필드에 포커스
+        FocusOnInputField();
+
+        // 경고 문구
+        warning = GetText((int)Texts.Warning_Text);
     }
 
 
@@ -59,11 +71,35 @@ public class Party_Create_Popup_UI : UI_Popup
         ClosePopupUI();
     }
 
+    // 인풋 필드에 포커스하는 메서드
+    private void FocusOnInputField()
+    {
+        partyNameInputField.OnPointerClick(new PointerEventData(EventSystem.current));
+    }
+
     // 파티 만들기 메서드
     private void PartyCreate(PointerEventData data)
     {
-        // TODO: 파티 만드는 코드 추가 필요
-        Debug.Log($"파티 이름: {partyNameInputField.text}");
+        warning.text = "";
+
+        // 파티 이름 입력 필드 비어있을 시 경고문구를 출력하고 메서드 종료
+        if (string.IsNullOrEmpty(partyNameInputField.text))
+        {
+            warning.text = "파티 이름을 입력해주세요.";
+            FocusOnInputField();
+            return;
+        }
+
+        // 파티 이름이 16자 초과일 경우 경고문구를 출력하고 메서드 종료
+        if (partyNameInputField.text.Length > 16)
+        {
+            warning.text = "파티 이름은 16자 이하로 설정해주세요.";
+            FocusOnInputField();
+            return;
+        }
+
+        // 파티 생성
+        Managers.Photon.MakeRoom(partyNameInputField.text);
 
         // 모든 Popup UI를 닫음
         CloseAllPopupUI();
