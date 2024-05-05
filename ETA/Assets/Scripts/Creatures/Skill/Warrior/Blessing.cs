439c4898bc1f5ba5ebe22c2b6a0ea907f3680764
wjdtpwls32 <wjdtpwls32@naver.com>
2024-05-05 17:31:39 +0900

    feat. skill_warrior_Blessing

---
+++ ETA/Assets/Scripts/Creatures/Skill/Warrior/Blessing.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blessing : Skill
+{
+    private Coroutine blessingCoroutine;
+    private ParticleSystem ps;
+    
+    protected override void Init()
+    {
+        SetCoolDownTime(15);
+        SkillType = Define.SkillType.Immediately;
+        base.Init();
+    }
+
+    public override IEnumerator StartSkillCast()
+    {
+        _animator.CrossFade("BUFF2", 0.1f);
+        Managers.Sound.Play("Skill/Heal");
+        ps = Managers.Effect.Play(Define.Effect.BlessingEffect, gameObject.transform);
+        _controller.Stat.Defense += 10;
+        Debug.Log($"Defense: {_controller.Stat.Defense}");
+
+        // 방어력 증가를 시작하고, 일정 시간 후에 감소시키는 코루틴을 시작합니다.
+        blessingCoroutine = StartCoroutine(BlessingCoroutine());
+
+        yield return new WaitForSeconds(0.5f);
+        _controller.ChangeState(_controller.MOVE_STATE);
+    }
+
+    private IEnumerator BlessingCoroutine()
+    {
+        // 방어력 증가 후 일정 시간 대기
+        yield return new WaitForSeconds(5.0f);
+
+        // 방어력 감소 및 파티클 중지
+        _controller.Stat.Defense -= 10;
+        Debug.Log($"Defense: {_controller.Stat.Defense}");
+        if (ps != null)
+            Managers.Effect.Stop(ps);
+    }
+}
