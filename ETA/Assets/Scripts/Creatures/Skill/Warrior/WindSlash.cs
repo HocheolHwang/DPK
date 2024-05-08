e589005c15cfd9a72f2f3206960f8373ccc123ac
신현기 <muffin3864@naver.com>
2024-05-08 23:46:37 +0900

    FEAT: collavo skill cyclone add

--- ETA/Assets/Scripts/Creatures/Skill/Warrior/WindSlash.cs
+++ ETA/Assets/Scripts/Creatures/Skill/Warrior/WindSlash.cs
@@ -12,7 +12,7 @@ public class WindSlash : Skill
         SkillType = Define.SkillType.Holding;
         skillRange = new Vector3(3, 3, 3);
         CollavoSkillRange = new Vector3(15, 15, 15);
-        //skillIcon.sprite = Resources.Load<Sprite>("Sprites/SkillIcon/Archer/WindBall.png");
+        //skillIcon.sprite = Resources.Load<Sprite>("Sprites/SkillIcon/Warrior/WindSlash.png");
         CollavoSkillName = "Cyclone";
     }
 
@@ -42,19 +42,43 @@ public class WindSlash : Skill
         _animator.CrossFade("COLLAVO", 0.1f);
         yield return new WaitForSeconds(0.5f);
 
+        ParticleSystem ps0 = Managers.Resource.Instantiate("Effect/CycloneUIEffect").GetComponent<ParticleSystem>();
+        ps0.Play();
+
+        ParticleSystem ps = Managers.Resource.Instantiate("Effect/WindSlash").GetComponent<ParticleSystem>();
+        ps.transform.position = gameObject.transform.position + transform.up;
+        ps.Play();
+
         ParticleSystem ps1 = Managers.Resource.Instantiate("Effect/CollavoCyclone").GetComponent<ParticleSystem>();
         ps1.transform.position = transform.position;
         ps1.Play();
 
         HitBox hitbox = Managers.Resource.Instantiate("Skill/HitBoxRect").GetComponent<HitBox>();
-        hitbox.SetUp(transform, Damage, -1, true);
+        hitbox.SetUp(transform, Damage);
         hitbox.transform.position = gameObject.transform.position + transform.forward * 4;
         hitbox.transform.localScale = CollavoSkillRange;
+
+        ParticleSystem ps2 = Managers.Resource.Instantiate("Effect/CollavoCycloneShot").GetComponent<ParticleSystem>();
+        ps2.transform.position = transform.position;
+
+        for (int i = 0; i < 10; i++)
+        {
+            yield return new WaitForSeconds(0.20f);
+            ps2.Play();
+            Managers.Resource.Destroy(hitbox.gameObject);
+            hitbox = Managers.Resource.Instantiate("Skill/HitBoxRect").GetComponent<HitBox>();
+            hitbox.transform.position = gameObject.transform.position + transform.forward * 4;
+            hitbox.transform.localScale = CollavoSkillRange;
+            Managers.Sound.Play("Skill/TargetSkill");
+        }
 
         yield return new WaitForSeconds(0.1f);
         Managers.Resource.Destroy(hitbox.gameObject);
         yield return new WaitForSeconds(1.0f);
+        Managers.Resource.Destroy(ps.gameObject);
+        Managers.Resource.Destroy(ps0.gameObject);
         Managers.Resource.Destroy(ps1.gameObject);
+        Managers.Resource.Destroy(ps2.gameObject);
 
         yield return new WaitForSeconds(0.1f);
         _controller.ChangeState(_controller.MOVE_STATE);
