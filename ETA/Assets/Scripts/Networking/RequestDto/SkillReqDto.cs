cc1f7638c72c5532c303a4bb2bcea2b4f60fe536
Su <suyoung998@naver.com>
2024-05-09 01:00:07 +0900

    Feat: Skill Dto 추가

---
+++ ETA/Assets/Scripts/Networking/RequestDto/SkillReqDto.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SkillReqDto
+{
+	public string message;
+	public string classCode;
+	public List<SkillRequestDto> skillList;
+}
+
+[System.Serializable]
+public class SkillRequestDto
+{
+	public int index;
+	public string skillCode;
+}
