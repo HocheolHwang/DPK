82fb47bbcf09567d46f515c9187d3242e93ffec1
최명성 <audtjd1893@gmail.com>
2024-05-12 21:59:08 +0900

    Feat: 모든 캐릭터 레벨 받아오기 API

---
+++ back/src/main/java/com/e207/back/dto/common/ClassDto.java
@@ -0,0 +1,12 @@
+package com.e207.back.dto.common;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ClassDto {
+    private int level;
+    private String classCode;
+}
--- back/src/main/java/com/e207/back/service/ClassService.java
+++ back/src/main/java/com/e207/back/service/ClassService.java
@@ -1,7 +1,9 @@
 package com.e207.back.service;
 
+import com.e207.back.dto.request.AllClassRequestDto;
 import com.e207.back.dto.request.CurrentClassRequestDto;
 import com.e207.back.dto.request.SelectClassRequestDto;
+import com.e207.back.dto.response.AllClassResponseDto;
 import com.e207.back.dto.response.CurrentClassResponseDto;
 
 import com.e207.back.dto.response.SelectClassResponseDto;
@@ -10,4 +12,5 @@ import org.springframework.http.ResponseEntity;
 public interface ClassService {
     ResponseEntity<? super CurrentClassResponseDto> getCurrentClass(CurrentClassRequestDto dto);
     ResponseEntity<? super SelectClassResponseDto> selectClass(SelectClassRequestDto dto);
+    ResponseEntity<? super AllClassResponseDto> getAllClass(AllClassRequestDto dto);
 }
