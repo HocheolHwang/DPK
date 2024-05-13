fe20d9adb84d89055f32a43882f1a88335870c94
HocheolHwang <inpofes7849@naver.com>
2024-05-13 20:36:27 +0900

    Feat: SkillSO Inspector 창 순서 변경

--- ETA/Assets/Scripts/SkillSO.cs
+++ ETA/Assets/Scripts/SkillSO.cs
@@ -9,22 +9,22 @@ public class SkillSO : ScriptableObject
     [Header("스킬 아이콘")]
     public Sprite Icon; // 스킬 아이콘
 
-    [Header("스킬 기본 정보")]
-    public string SkillCode; // 스킬 코드
-    public string SkillName; // 스킬 영어 이름
-    public string SkillKoreanName; // 스킬 한글 이름
-    [TextArea]
-    public string SkillDescription; // 스킬 설명
+    [Header("스킬 종류")]
+    public bool IsCollavo; // 콜라보 스킬 여부
+    public bool IsCounter; // 카운터 스킬 여부
+    public Define.SkillType SkillType; // 스킬 타입
 
     [Header("스킬 상세 정보")]
     public int RequiredLevel; // 습득 필요 레벨
     public int Damage; // 스킬 피해량
     public float CoolDownTime; // 스킬 쿨타임
 
-    [Header("스킬 종류")]
-    public bool IsCollavo; // 콜라보 스킬 여부
-    public bool IsCounter; // 카운터 스킬 여부
-    public Define.SkillType SkillType; // 스킬 타입
+    [Header("스킬 기본 정보")]
+    public string SkillCode; // 스킬 코드
+    public string SkillName; // 스킬 영어 이름
+    public string SkillKoreanName; // 스킬 한글 이름
+    [TextArea]
+    public string SkillDescription; // 스킬 설명
 
     [Header("콜라보 스킬 정보")]
     public string CollavoSkillName; // !!!!주의!!! 콜라보를 함께 발동할 스킬의 영어 이름
