8aa856cca5c7b4ced88b97fd32576c1b0659b832
HocheolHwang <inpofes7849@naver.com>
2024-05-14 12:08:50 +0900

    Feat: 스킬 창 업데이트 기능 추가

--- ETA/Assets/Scripts/UI/Sub Item/Skill_Select.cs
+++ ETA/Assets/Scripts/UI/Sub Item/Skill_Select.cs
@@ -10,19 +10,12 @@ public class Skill_Select : MonoBehaviour
 
     // --------------- 스킬 목록 ---------------
 
-    // 계열별 스킬 이미지를 저장하는 딕셔너리
-    //private Dictionary<Image, string, int> skillList = new Dictionary<Image, string, int>
-    //{
-    //    { Image, string, int a},
-    //    { Image, string, int a},
-    //    { Image, string, int a},
-    //    { Image, string, int a},
-    //    { Image, string, int a},
-    //    { Image, string, int a},
-    //    { Image, string, int a},
-    //    { Image, string, int a},
-    //    { Image, string, int a},
-    //};
+    // 스킬 목록 정보
+    private Button[] skillContainers = new Button[16]; // 스킬 버튼
+    private Image[] skillIcons = new Image[16]; // 스킬 아이콘
+    private Image[] skillCanNotBeUsedIcons = new Image[16]; // 스킬 비활성화 아이콘
+    private TMP_Text[] skillNames = new TMP_Text[16]; // 스킬 이름
+    private TMP_Text[] skillRequiredLevels = new TMP_Text[16]; // 필요 레벨
 
 
     // --------------- 스킬 슬롯 ---------------
@@ -39,27 +32,59 @@ public class Skill_Select : MonoBehaviour
     // 선택된 캐릭터 코드 이전 값
     private string previousSelectedClassCode;
 
+    // 클래스 코드와 스킬 이름 배열을 매핑하는 딕셔너리
+    Dictionary<string, string[]> classSkills = new Dictionary<string, string[]>()
+{
+    {"C001",new string[]
+        {
+            "DrawSword", "Guard", "Massacre", "Guard",
+            "Sting", "SwordWave", "TelekineticSwords", "Whirlwind",
+            "WindSlash", "Guard", "Guard", "Guard",
+            "Guard", "Guard", "Guard", "Guard"
+        }
+    },
+    {"C002", new string[]
+        {
+            "ArrowBomb", "ArrowShower", "ArrowStab", "ForestSpirit",
+            "LightningShot", "RapidArrow", "WindBall", "WindShield",
+            "WindShield", "WindShield", "WindShield", "WindShield",
+            "WindShield", "WindShield", "WindShield", "WindShield"
+        }
+    },
+    {"C003", new string[]
+         {
+            "Tornado", "Thunder", "Rage", "QuickFreeze",
+            "Protection", "Meteor", "Infusion", "IceBone",
+            "Heal", "Gravity", "FlashLight", "BloodBoom",
+            "Adrenalin", "Adrenalin", "Adrenalin", "Adrenalin"
+        }
+    }
+};
 
     // ------------------------------ UI 초기화 ------------------------------
 
     private void Start()
     {
+        // --------------- 캐릭터 정보 초기화 ---------------
+
+        // 선택된 캐릭터 코드 초기화
+        selectedClassCode = Character_Select.selectedClassCode;
+
+        // 선택된 캐릭터 코드의 초기 이전 값 설정
+        previousSelectedClassCode = selectedClassCode;
+
+
         // --------------- 스킬 목록 ---------------
 
-        // 스킬 계열 텍스트 오브젝트 연결
-        //for (int i = 0; i < 4; i++)
-        //{
-        //    skillCategoryTexts[i] = GameObject.Find($"Category_{i + 1}_Skill_Text").GetComponent<TMP_Text>();
-        //}
-
-        // 스킬 이미지 오브젝트 연결
-        //for (int category = 1; category <= 4; category++)
-        //{
-        //    for (int i = 0; i < 6; i++)
-        //    {
-        //        categorySkillIconsByCategory[category][i] = GameObject.Find($"Category_{category}_Skill_{i + 1}").GetComponent<Image>();
-        //    }
-        //}
+        // 스킬 목록 정보 연결
+        for (int i = 0; i < 16; i++)
+        {
+            skillContainers[i] = GameObject.Find($"Skill_{i + 1}_Container").GetComponent<Button>();
+            skillIcons[i] = GameObject.Find($"Skill_{i + 1}_Icon").GetComponent<Image>();
+            skillCanNotBeUsedIcons[i] = GameObject.Find($"Skill_{i + 1}_Can_Not_Be_Used").GetComponent<Image>();
+            skillNames[i] = GameObject.Find($"Skill_{i + 1}_Name").GetComponent<TMP_Text>();
+            skillRequiredLevels[i] = GameObject.Find($"Skill_{i + 1}_Required_Level").GetComponent<TMP_Text>();
+        }
 
 
         // --------------- 스킬 슬롯 ---------------
@@ -71,19 +96,13 @@ public class Skill_Select : MonoBehaviour
         }
 
 
-        // --------------- 캐릭터 정보 초기화 ---------------
-
-        // 선택된 캐릭터 코드 초기화
-        selectedClassCode = Character_Select.selectedClassCode;
-
-        // 선택된 캐릭터 코드의 초기 이전 값 설정
-        previousSelectedClassCode = selectedClassCode;
-
-
         // --------------- 스킬 정보 업데이트---------------
 
         // 모든 스킬 정보 업데이트
         UpdateAllSkillInfo(selectedClassCode);
+
+        // 선택한 스킬 정보 업데이트
+        UpdateSelectedSkillInfo(selectedClassCode);
     }
 
 
@@ -100,6 +119,9 @@ public class Skill_Select : MonoBehaviour
             // 모든 스킬 정보 업데이트
             UpdateAllSkillInfo(selectedClassCode);
 
+            // 선택한 스킬 정보 업데이트
+            UpdateSelectedSkillInfo(selectedClassCode);
+
             // 이전 selectedClassCode 값을 현재 값으로 업데이트
             previousSelectedClassCode = selectedClassCode;
         }
@@ -107,40 +129,6 @@ public class Skill_Select : MonoBehaviour
 
 
     // ------------------------------ 메서드 정의 ------------------------------
-
-    // 스킬 계열 텍스트 업데이트 메서드
-    //private void UpdateSkillCategoryTexts(string selectedClassCode)
-    //{
-    //    if (skillCategoriesByClassCode.TryGetValue(selectedClassCode, out string[] categories))
-    //    {
-    //        for (int i = 0; i < skillCategoryTexts.Length; i++)
-    //        {
-    //            skillCategoryTexts[i].text = categories[i];
-    //        }
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("알 수 없는 클래스 코드: " + selectedClassCode);
-    //    }
-    //}
-
-    // 계열별 스킬 이미지 업데이트 메서드
-    private void UpdateCategorySkillIcons()
-    {
-
-    }
-
-    // 공용 스킬 이미지 업데이트 메서드
-    private void UpdateCommonSkillIcons()
-    {
-
-    }
-
-    // 스킬 슬롯 이미지 업데이트 메서드
-    private void UpdateSlotSkillIcons()
-    {
-
-    }
 
     // 모든 스킬 정보를 업데이트하는 메서드
     private void UpdateAllSkillInfo(string classCode)
@@ -152,16 +140,114 @@ public class Skill_Select : MonoBehaviour
             return;
         }
 
-        // 스킬 계열 텍스트 업데이트
-        // UpdateSkillCategoryTexts(classCode);
+        // 스킬 목록 업데이트
+        UpdateSkillList(classCode);
 
-        // 계열별 스킬 이미지 업데이트
-        UpdateCategorySkillIcons();
+        // 스킬 슬롯 업데이트
+        UpdateSkillSlot(classCode);
+    }
 
-        // 공용 스킬 이미지 업데이트
-        UpdateCommonSkillIcons();
+    // 스킬 목록 업데이트 메서드
+    private void UpdateSkillList(string classCode)
+    {
+        // 플레이어 레벨 가져오기
+        int playerLevel = Managers.Player.GetLevel();
 
-        // 스킬 슬롯 이미지 업데이트
-        UpdateSlotSkillIcons();
+        for (int i = 0; i < 16; i++)
+        {
+            // SkillSO 객체 로드
+            SkillSO skillData = Resources.Load<SkillSO>($"Scriptable/Skill/{classSkills[classCode][i]}");
+            if (skillData == null) continue;
+
+            skillIcons[i].sprite = skillData.Icon;
+            skillNames[i].text = skillData.SkillKoreanName;
+            skillRequiredLevels[i].text = $"필요레벨 {skillData.RequiredLevel}";
+
+            // 필요 레벨이 플레이어 레벨보다 클 경우 텍스트 색상을 빨간색으로, 그렇지 않으면 흰색으로 설정
+            if (skillData.RequiredLevel > playerLevel)
+            {
+                skillCanNotBeUsedIcons[i].gameObject.SetActive(true);
+                skillNames[i].color = new Color(1, 0, 0, 0.8f);
+                skillRequiredLevels[i].color = new Color(1, 0, 0, 0.8f);
+            }
+            else
+            {
+                skillCanNotBeUsedIcons[i].gameObject.SetActive(false);
+                skillNames[i].color = Color.white;
+                skillRequiredLevels[i].color = Color.white;
+            }
+        }
+    }
+
+    // 스킬 슬롯 아이콘 업데이트 메서드
+    public void UpdateSkillSlot(string classCode)
+    {
+        SkillInfo[] skills = classCode switch
+        {
+            "C001" => Managers.Player.warriorSkills,
+            "C002" => Managers.Player.archerSkills,
+            "C003" => Managers.Player.mageSkills,
+            _ => null
+        };
+
+        if (skills == null)
+        {
+            // 스킬이 없는 경우 모든 슬롯을 기본 이미지로 설정
+            FillRemainingSlots(0);
+            return;
+        }
+
+        // 스킬 개수만큼 반복
+        for (int i = 0; i < skills.Length; i++)
+        {
+            SkillSO skillData = Resources.Load<SkillSO>($"Scriptable/Skill/{skills[i].skillName}");
+
+            // 이미지 컴포넌트에 Sprite 할당
+            if (skillData != null)
+            {
+                slotSkillIcons[i].sprite = skillData.Icon;
+            }
+        }
+
+        // 남은 슬롯들을 기본 이미지로 채우기
+        if (skills.Length < slotSkillIcons.Length)
+        {
+            FillRemainingSlots(skills.Length);
+        }
+    }
+
+    // 남은 슬롯들을 기본 이미지로 채우는 메서드
+    private void FillRemainingSlots(int startIndex)
+    {
+        for (int i = startIndex; i < slotSkillIcons.Length; i++)
+        {
+            if (slotSkillIcons[i] != null)
+            {
+                slotSkillIcons[i].sprite = Resources.Load<Sprite>("Sprites/Skill Slot/Skill Cooldown Image");
+            }
+        }
+    }
+
+    // 선택한 스킬 정보 업데이트 메서드
+    public void UpdateSelectedSkillInfo(string classCode)
+    {
+        if (classCode == null || !classSkills.ContainsKey(classCode))
+        {
+            Debug.LogError("Invalid classCode: " + classCode);
+            return;
+        }
+
+        // 스킬 목록 정보 연결
+        for (int i = 0; i < 16; i++)
+        {
+            // 클래스 코드에 따른 스킬 이름 가져오기
+            string skillName = classSkills[classCode][i];
+
+            // Skill_Info 컴포넌트 참조
+            Skill_Info skillInfoComponent = GameObject.Find("Skill_Info").GetComponent<Skill_Info>();
+
+            // 버튼 클릭 이벤트 추가
+            skillContainers[i].onClick.AddListener(() => skillInfoComponent.UpdateSkillInfo(skillName));
+        }
     }
 }
