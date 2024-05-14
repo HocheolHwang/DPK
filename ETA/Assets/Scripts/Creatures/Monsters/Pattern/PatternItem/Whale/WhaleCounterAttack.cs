dd64c6f11b9f7ce42da12928ee9a6b251561a6b4
hwalang <smdong97@gmail.com>
2024-05-14 17:39:38 +0900

    Fix: 사용하지 않는 변수 제거

--- ETA/Assets/Scripts/Creatures/Monsters/Pattern/PatternItem/Whale/WhaleCounterAttack.cs
+++ ETA/Assets/Scripts/Creatures/Monsters/Pattern/PatternItem/Whale/WhaleCounterAttack.cs
@@ -6,7 +6,6 @@ public class WhaleCounterAttackPattern : Pattern
 {
     [Header("개발 편의성")]
     [SerializeField] float _hitboxForwardLoc = 0.3f;
-    [SerializeField] float _hitboxRadius = 6.0f;
     [SerializeField] float _upLoc = 1.0f;
     [SerializeField] float _rightLoc = 0f;
     [SerializeField] Vector3 _hitboxRange = new Vector3(7f, 7f, 7f);
