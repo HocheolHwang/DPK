bad43d1e951cebec2a57a2824d19b14d96af1354
최명성 <audtjd1893@gmail.com>
2024-05-16 17:40:51 +0900

    Feat: 레벨 보정 추가

--- ETA/Assets/Scripts/Creatures/Player/PlayerStat.cs
+++ ETA/Assets/Scripts/Creatures/Player/PlayerStat.cs
@@ -1,7 +1,31 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStat : Stat
 {
+    private void Start()
+    {
+        PhotonView photonView = GetComponent<PhotonView>();
+
+        foreach(var player in PhotonNetwork.PlayerList)
+        {
+            if(player.CustomProperties.TryGetValue("viewID", out object vid))
+            {
+                int viewId = (int)vid;
+                if(photonView.ViewID == viewId)
+                {
+                    if(player.CustomProperties.TryGetValue("PlayerLevel", out object level))
+                    {
+                        int playerLevel = (int)level;
+                        MaxHp += 20 * playerLevel;
+                        Hp += 20 * playerLevel;
+                        AttackDamage += 5 * playerLevel;
+                    }
+                }
+            }
+            
+        }
+    }
 }
