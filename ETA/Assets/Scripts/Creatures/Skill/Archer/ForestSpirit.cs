e6459daa6c53773cf12dcd1eb1908410b30ae958
lement3864 <lement3864@gmail.com>
2024-05-17 12:27:56 +0900

    FEAT: stage 2 balance add

--- ETA/Assets/Scripts/Creatures/Skill/Archer/ForestSpirit.cs
+++ ETA/Assets/Scripts/Creatures/Skill/Archer/ForestSpirit.cs
@@ -33,7 +33,7 @@ public class ForestSpirit : Skill
     }
 
     IEnumerator ForestSpiritCoroutine()
-    {
+    {   
         ParticleSystem ps01 = Managers.Effect.Play(Define.Effect.ForestSpiritSpawn, 1.0f, gameObject.transform);
         ps01.transform.position = transform.position;
         //ps01.Play();
@@ -46,6 +46,7 @@ public class ForestSpirit : Skill
         Vector3 spawnPosition = _skillSystem.TargetPosition;
 
         if (PhotonNetwork.IsMasterClient) PhotonNetwork.Instantiate("Prefabs/Creatures/Player/ForestSpirit", spawnPosition, Quaternion.identity);
+            
 
         // 지정 위치에 소환
         Managers.Sound.Play("Skill/ForestSpiritSpawn");
