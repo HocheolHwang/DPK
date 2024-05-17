eaf7bec93ada693e1f141aef2c3e6904f1a8880d
장수영 <suyoung998@naver.com>
2024-05-17 14:56:55 +0900

    Fix: 드라이어드 카운터 시간 감소

--- ETA/Assets/Scripts/Creatures/States/FlowerDryad/FlowerDryadState.cs
+++ ETA/Assets/Scripts/Creatures/States/FlowerDryad/FlowerDryadState.cs
@@ -16,7 +16,7 @@ public class FlowerDryadState : State
 {
     protected static int counterTimeTrigger = 1;        // 처음 플레이어를 직면한 후에 counterTime을 계산하기 시작한다.
     protected static float counterTime = 0;             // 카운터 공격을 할 수 있는 시간을 관리
-    protected const float threadHoldCounter = 15.0f;    // 15초마다 카운터 패턴 공격
+    protected const float threadHoldCounter = 8.0f;    // 8초마다 카운터 패턴 공격
 
     protected FlowerDryadController _controller;
     protected FlowerDryadAnimationData _animData;
