da8cee938eaa97d5a02e3ffabc26384fbbe66a21
최명성 <audtjd1893@gmail.com>
2024-05-17 15:22:14 +0900

    Fix: 던전 랭킹 갯수 5개로 변경

--- back/src/main/java/com/e207/back/repository/DungeonLogRepository.java
+++ back/src/main/java/com/e207/back/repository/DungeonLogRepository.java
@@ -2,17 +2,13 @@ package com.e207.back.repository;
 
 import com.e207.back.entity.DungeonEntity;
 import com.e207.back.entity.DungeonLogEntity;
-import com.e207.back.entity.PlayerClassEntity;
 import com.e207.back.entity.id.DungeonLogId;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface DungeonLogRepository extends JpaRepository<DungeonLogEntity, DungeonLogId> {
 
-    List<DungeonLogEntity> findTop3ByDungeonAndIsClearedTrueOrderByClearTimeAscCreatedAtAsc(DungeonEntity dungeon);
+    List<DungeonLogEntity> findTop5ByDungeonAndIsClearedTrueOrderByClearTimeAscCreatedAtAsc(DungeonEntity dungeon);
 
 }
