b7dd8a8fe756c784c8b0daec3979bc7a9ba968da
HocheolHwang <inpofes7849@naver.com>
2024-05-20 14:39:40 +0900

    Feat: 서버 선택 팝업 UI 추가

---
+++ ETA/Assets/Scripts/UI/Popup/Server_Selection_Popup_UI.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Server_Selection_Popup_UI : UI_Popup
+{
+    // ------------------------------ 변수 정의 ------------------------------
+
+    // 열거형 정의
+    enum Buttons
+    {
+        // 서버 버튼
+        Server_1_Button,
+        Server_2_Button,
+        Server_3_Button,
+
+        // 서버 선택 버튼
+        Server_Selection_Button,
+
+        // 게임 종료 버튼
+        Game_Exit_Button
+    }
+
+    // UI 컴포넌트 바인딩 변수
+    private Button[] serverButtons = new Button[3];
+    private Button serverSelectionButton;
+    private Button gameExitButton;
+
+    // 선택된 서버를 확인하는 변수
+    private int CurrentServerNumber
+    {
+        get { return _currentServerNumber; }
+        set
+        {
+            _currentServerNumber = value;
+            SelectServer(_currentServerNumber);
+        }
+    }
+
+    // 실제 변수 값
+    private int _currentServerNumber;
+
+
+    // ------------------------------ UI 초기화 ------------------------------
+    public override void Init()
+    {
+        // 기본 초기화
+        base.Init();
+
+        // 컴포넌트 바인딩
+        Bind<Button>(typeof(Buttons));
+
+        // 서버 선택 버튼을 반복문으로 처리
+        for (int i = 0; i < 3; i++)
+        {
+            int serverNumber = i;
+            serverButtons[i] = GetButton((int)Buttons.Server_1_Button + i);
+            AddUIEvent(serverButtons[i].gameObject, (PointerEventData data) => SelectServer(serverNumber));
+        }
+
+        // 서버 선택 버튼 이벤트 등록
+        serverSelectionButton = GetButton((int)Buttons.Server_Selection_Button);
+        AddUIEvent(serverSelectionButton.gameObject, ServerSelection);
+        AddUIKeyEvent(serverSelectionButton.gameObject, () => ServerSelection(null), KeyCode.Return);
+
+        // 게임 종료 버튼 이벤트 등록
+        gameExitButton = GetButton((int)Buttons.Game_Exit_Button);
+        AddUIEvent(gameExitButton.gameObject, OpenGameExit);
+        AddUIKeyEvent(gameExitButton.gameObject, () => OpenGameExit(null), KeyCode.Escape);
+
+        // 서버 선택 상태 초기화
+        serverButtons[0].Select();
+        serverButtons[0].onClick.Invoke();
+    }
+
+
+    // ------------------------------ 메서드 정의 ------------------------------
+
+    // 선택한 서버 번호 업데이트 메서드
+    private void SelectServer(int serverNumber)
+    {
+        _currentServerNumber = serverNumber;
+    }
+
+    // 서버 선택 메서드
+    private void ServerSelection(PointerEventData data)
+    {
+        // TODO: currentServerNumber 숫자에 따라 서로 다른 서버로 접속하는 코드 작성 필요
+
+
+
+        // 서버 선택이 성공적으로 이루어졌을 경우
+
+        // 현재 Popup UI를 닫음
+        ClosePopupUI();
+
+        // 로그인 Popup UI를 띄움
+        Managers.UI.ShowPopupUI<Login_Popup_UI>("[Login]_Login_Popup_UI");
+    }
+
+    // 게임 종료 Popup UI 띄우기 메서드
+    private void OpenGameExit(PointerEventData data)
+    {
+        // 게임 종료 Popup UI를 띄움
+        Managers.UI.ShowPopupUI<Game_Exit_Popup_UI>("[Common]_Game_Exit_Popup_UI");
+    }
+}
